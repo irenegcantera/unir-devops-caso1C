b027f40f225bfa8198356c37ad1d1042b87b1c02
Irene <66362460+irenegcantera@users.noreply.github.com>
2024-02-18 16:14:16 +0100

    Update setup.sh

--- pipelines/PIPELINE-FULL-STAGING/setup.sh
+++ pipelines/PIPELINE-FULL-STAGING/setup.sh
@@ -5,17 +5,7 @@ python3.8 -m venv todo-list-aws
 source todo-list-aws/bin/activate
 python -m pip install --upgrade pip
 #For static testing
-#python -m pip install radon
 python -m pip install flake8
-#python -m pip install flake8-polyfill
 python -m pip install bandit
 #For integration testing
 python -m pip install pytest
-#python -m pip install request
-#For unit testing
-#python -m pip install boto3
-#python -m pip install moto
-#python -m pip install mock==4.0.2
-#python -m pip install coverage==4.5.4
-
-#pwd
